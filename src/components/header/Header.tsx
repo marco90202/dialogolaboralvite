46780956e4f2848742fd34eac6548f8d330f4fe9
heyden1976 <heyden1976@hotmail.com>
2022-10-07 18:37:58 -0500

    (develop):cabecera

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Button, Container, Menu, Menu_items, Menu_li, Logo} from './styles';
+import hamburguerMenu from '../../assets/icons/hamburgerMenu.svg';
 
 const Header = () => {
 
 
     return(
-        <div>HEADER</div>
+       
+            <Container>
+                <Menu>
+                    <Logo>dialogo laboral</Logo>
+                    <img className="Icono"style={{width: '40px', marginRight: '20px'}} src={hamburguerMenu} alt='iconmenu'/>
+                        <Menu_items>
+                                <Menu_li>inicio</Menu_li>
+                                <Menu_li>nosotros</Menu_li>
+                                <Menu_li>servicios</Menu_li>
+                                <Menu_li>eventos</Menu_li>
+                                <Menu_li>contactenos</Menu_li>
+                        </Menu_items>
+                </Menu>
+            </Container>
+        
     )
 }
 export default Header
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -2,9 +2,76 @@ import styled from 'styled-components';
 
 const Button = styled.button`
     padding: 150px;
-    background: red;
+    background:blue;
+`;
+
+const Container = styled.div`
+width: 100%;
+height: 100%;
+
+
+@media (min-width: 992px) {
+    .Icono {
+    display:none;
+}
+}
+`;
+
+const Logo = styled.h2`
+text-transform:uppercase;
+color: #fff;
+padding: 10px;
+
+:hover {
+    background:rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+    
+}
+`;
+
+const Menu = styled.nav`
+width: 100%;
+height: 90px;
+padding: 0 10px;
+ background-color: #14547c;
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ border-bottom: 1px solid #fff;
+  box-shadow: 5px 0 10px rgba(0, 0, 0, 0.6);
+  position: fixed;
+
+`;
+
+const Menu_items = styled.ul`
+  display: flex;
+  margin-right: 10px;
+  margin-top: 20;
+  list-style: none;
+  justify-content: space-around;
+  display: none;
+
+  @media (min-width: 992px) {
+    display: flex;
+    
+  }
+`;
+
+const Menu_li = styled.li`
+ border-radius: 3px;
+ padding: 10px 23px;
+ font-weight: 300;
+  font-size: 20px;
+  list-style: none;
+  text-transform:uppercase;
+  color: #fff;
+
+  :hover {
+    background: rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+  }
 `;
 
 export {
-    Button
+    Button, Container, Menu, Menu_items, Menu_li, Logo
 }
